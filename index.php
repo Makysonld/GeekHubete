cd27177b65da380f33c6d3728333bc8abc1dc3f9
Jose E <kingsonsk666@gmail.com>
2022-12-05 20:15:25 -0300

    Teste+Atualização

--- index.php
+++ index.php
@@ -1,17 +1,62 @@
 <?php
-echo "ola mundo que bosta"
 
+    $paginas = ['home'=>'Bem vindo a página ','sobre'=>'Site em desenvolvimento sobre a cultura Geek.', 'contato'=>''];
+
+    $paginas['contato'] = '<form><input type="text" placeholder= "seu nome..." /></form>';
 ?>  
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>GeekHubETE</title>
+    <style type= "text/css">    
+         *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+         }
+
+         header{
+            background-color:#069;
+            padding: 8px 10px;
+            text-align: center;
+         }
+
+         a{
+            display: inline-block;
+            margin: 0 20px;
+            color: black;
+            font-size: 17px ;
+
+         }
+         </style>
+
 </head>
 <body>
-    
+    <header>
+        <?php   
+            foreach ($paginas as $key => $value) {
+               echo '<a href="?page='.$key.'">'. ucfirst($key).'</a>';
+            }
+        ?>
+
+    </header>
+    <section>
+        <h2>
+            <?php
+
+            $pagina = (isset($_GET['page']) ? $_GET['page'] : 'home');
+
+                if (!array_key_exists($pagina, $paginas)){
+                    $pagina = 'home';
+                }
+
+                echo ucfirst($pagina);
+
+            ?>
+        </h2>
+        <p><?php echo $paginas[$pagina]; ?>
+        </p>
+    </section>
 </body>
 </html>
